3684f0556c2e6517a8081592370bee7effd4d490
Maan9 <wsmaan896@gmail.com>
2024-09-13 17:20:19 +0100

    test

--- src/main/java/java_test.java
+++ src/main/java/java_test.java
@@ -1,5 +1,8 @@
+package src.main.java;
+
 public class java_test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("test ");
+
     }
 }
