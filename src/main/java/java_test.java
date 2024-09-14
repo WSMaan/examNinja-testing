a49f9156597141ff090f30c73866a93a94f70e1a
Maan9 <wsmaan896@gmail.com>
2024-09-14 22:32:06 +0100

    test

--- src/main/java/java_test.java
+++ src/main/java/java_test.java
@@ -2,7 +2,7 @@ package src.main.java;
 
 public class java_test {
     public static void main(String[] args) {
-        System.out.println("test ");
+        System.out.println("test");
 
     }
 }
